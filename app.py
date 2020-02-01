d556df2b5c831f3728e8747b6e940b08ce75306e
shellkore <shellkoresahu@gmail.com>
2020-02-02 01:04:07 +0530

    db created successfuly

--- app.py
+++ app.py
@@ -9,15 +9,31 @@ def dbHandler():
 	con = sql.connect("database.db")
 	cur=con.cursor()
 	cur.execute("select name from host")
+	return
+
+def registerInDb(name,gender,email,hostel,room,institute):
+
 
 @app.route('/',methods = ['GET'])
 def home():
 	return render_template('index.html')
 
-@app.route('/',methods = ['GET,POST'])
+@app.route('/register',methods = ['GET','POST'])
 def register():
-	if method == 'GET':
-		return render_template(register.html)
+	if request.method == 'GET':
+		return render_template('register.html')
+	else:
+		username = request.form['username']
+		password = request.form['password']
+		name = request.form['firstname']
+		gender = request.form['gender']
+		email = request.form['email']
+		hostel = request.form['hostel-name']
+		room = request.form['room-no']
+		institute = request.form['institute-name']	
+
+		msg = 'registered successfully'
+		return render_template('result.html',msg=msg)
 
 if __name__=='__main__':
 	app.run(host='0.0.0.0', debug = True, port = 5000)
--- createDB.py
+++ createDB.py
@@ -3,14 +3,19 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print ("Opened database successfully")
 
-conn.execute('CREATE TABLE host (name TEXT, email TEXT, phone INTEGER NOT NULL PRIMARY KEY,address TEXT)')
-print ("Table host created successfully")
+# conn.execute('CREATE TABLE host (name TEXT, email TEXT, phone INTEGER NOT NULL PRIMARY KEY,address TEXT)')
+# print ("Table host created successfully")
 
-conn.execute('CREATE TABLE visitor (name TEXT, email TEXT, phone INTEGER NOT NULL PRIMARY KEY, host TEXT, checkin TEXT, checkout TEXT)')
-print("table visitor created successfully")
+# conn.execute('CREATE TABLE visitor (name TEXT, email TEXT, phone INTEGER NOT NULL PRIMARY KEY, host TEXT, checkin TEXT, checkout TEXT)')
+# print("table visitor created successfully")
+# conn.close()
+
+conn.execute('CREATE TABLE user (username TEXT,password TEXT, name TEXT, gender TEXT,email TEXT, hostel TEXT, room TEXT, institute TEXT)')
+print("user table created successfully!")
+
+conn.execute('CREATE TABLE reciept(username TEXT,recieptID TEXT,shirts INTEGER,jeans INTEGER,hoodies INTEGER,sheets INTEGER)')
+print("reciept table  created succefully!")
 conn.close()
-
-
 #firstname
 #gender
 #email
